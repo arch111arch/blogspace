556ef36542eac372efa780b0857e1e2104dd15d5
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2023-11-20 10:56:30 +0000

    Create new Item model in models.py. Migrate to database. Test fuctionality in the server.

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):#inherit the functions inside Models to use in the Item()
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+    #a field can not be created it it is empty or without text.
+    #it will be checked as not done by default.
+    
