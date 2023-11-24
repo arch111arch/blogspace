4a3a182ef0f5946aa89494b6ccfad592482df366
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2023-11-24 10:51:16 +0000

    create forms.py. In forms.py, import Item and create the class ItemForm.

---
+++ todo/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Item
+
+
+#create a class that inherits django functionality
+class ItemForm(forms.Modelform):
+    class Meta:
+        model = Item
+        fields = ['name', 'done']
